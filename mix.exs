4b839448ad0aefddaaea408fe12128dc2ba25480
Kishan B <4175339+kishb87@users.noreply.github.com>
2026-06-30 17:31:44 -0500

    Relax tailwind dep to >= 0.2.0 for compatibility

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule LivePane.MixProject do
       {:bandit, "~> 1.5", only: :dev},
       {:credo, ">= 0.0.0", only: [:dev], runtime: false},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
+      {:tailwind, ">= 0.2.0", runtime: Mix.env() == :dev},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
